30ae83a45ef2db0d91522177ff9f65f62f52d02d
oleonb92 <osmanileon92@gmail.com>
2025-05-20 18:32:08 -0500

    Fix STATIC_ROOT path for static files on Render

--- backend/lynkledger_api/settings.py
+++ backend/lynkledger_api/settings.py
@@ -190,7 +190,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/app/staticfiles'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
