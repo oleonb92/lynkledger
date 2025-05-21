e3af2dc4d909922054e6a9e1ad358a2d024981aa
oleonb92 <osmanileon92@gmail.com>
2025-05-20 22:50:39 -0500

    Add app_name to accounting URLs

--- backend/accounting/urls.py
+++ backend/accounting/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
+app_name = 'accounting'
+
 router = DefaultRouter()
 router.register(r'accounts', views.AccountViewSet, basename='account')
 router.register(r'transactions', views.TransactionViewSet, basename='transaction')
